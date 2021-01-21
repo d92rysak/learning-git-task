a70f3998538d4ba6d47bafebb5b1be6957b0daa6
Damian Rysak <d92rysak@gmail.com>
2021-01-21 17:21:11 +0100

    loop added

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,10 @@ shopping_dict = {
     "piekarnia": ["chleb","pączek","bułki"],
     "warzywniak": ["marchew","seler","rukola"]
 }
+items_quantity=0
+for sklep in shopping_dict:
+    """Shopping_list=[]
+    for item in shopping_dict[sklep] : 
+        item=item.title()   
+        Shopping_list.append(item)"""
+    print(f"Idę do {sklep.title()} i kupuję tu następujące rzeczy: {shopping_dict[sklep]}")
