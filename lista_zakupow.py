15efe1ef61e859ea98c4fbec3e636abafc3a1fb5
Damian Rysak <d92rysak@gmail.com>
2021-01-21 17:23:41 +0100

    loop update-quantity added

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -9,3 +9,5 @@ for sklep in shopping_dict:
         item=item.title()   
         Shopping_list.append(item)"""
     print(f"Idę do {sklep.title()} i kupuję tu następujące rzeczy: {shopping_dict[sklep]}")
+    items_quantity=items_quantity+len(shopping_dict[sklep])
+print(f"W sumie kupuję {items_quantity} produktów")
